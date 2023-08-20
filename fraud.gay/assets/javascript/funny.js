ac97cbdbbfaf84006f46c23713f256902caa9fe6
setfenv <130825965+chicagogrimreaper@users.noreply.github.com>
2023-08-20 23:18:28 +0300

    src
    xd

---
+++ fraud.gay/assets/javascript/funny.js
@@ -0,0 +1,31 @@
+// deobfuscated by sturmgeist
+const body = document.getElementsByTagName("body");
+window.addEventListener("keydown", event => {
+  if (event.ctrlKey && (event.key === "S" || event.key === "s")) {
+    event.preventDefault();
+    body[0].innerHTML = "quit trying to get the source you fat fuck, discord.gg/desperate winning fuck nigga https://fraud.gay xeno owns you faggots";
+  }
+  if (event.ctrlKey && event.key === "C") {
+    event.preventDefault();
+    body[0].innerHTML = "quit trying to get the source you fat fuck, discord.gg/desperate winning fuck nigga https://fraud.gay xeno owns you faggots";
+  }
+  if (event.ctrlKey && (event.key === "E" || event.key === "e")) {
+    event.preventDefault();
+    body[0].innerHTML = "quit trying to get the source you fat fuck, discord.gg/desperate winning fuck nigga https://fraud.gay xeno owns you faggots";
+  }
+  if (event.ctrlKey && (event.key === "I" || event.key === "i" || event.key === "Ä±")) {
+    event.preventDefault();
+    body[0].innerHTML = "quit trying to get the source you fat fuck, discord.gg/desperate winning fuck nigga https://fraud.gay xeno owns you faggots";
+  }
+  if (event.ctrlKey && (event.key === "K" || event.key === "k")) {
+    event.preventDefault();
+    body[0].innerHTML = "quit trying to get the source you fat fuck, discord.gg/desperate winning fuck nigga https://fraud.gay xeno owns you faggots";
+  }
+  if (event.ctrlKey && (event.key === "U" || event.key === "u")) {
+    event.preventDefault();
+    body[0].innerHTML = "quit trying to get the source you fat fuck, discord.gg/desperate winning fuck nigga https://fraud.gay xeno owns you faggots";
+  }
+});
+document.addEventListener("contextmenu", function (_0x7d44x3) {
+  _0x7d44x3.preventDefault();
+});
---
+++ fraud.gay/assets/javascript/main.js
@@ -0,0 +1,33 @@
+// deobfuscated by sturmgeist
+console.log('Credit to @fourdevils for stealing the page and to sturmgeist aka zirt for deobfuscation');
+console.log('Credit to @fourdevils for stealing the page and to sturmgeist aka zirt for deobfuscation');
+console.log('Credit to @fourdevils for stealing the page and to sturmgeist aka zirt for deobfuscation');
+console.log('Credit to @fourdevils for stealing the page and to sturmgeist aka zirt for deobfuscation');
+console.log('Credit to @fourdevils for stealing the page and to sturmgeist aka zirt for deobfuscation');
+console.log('Credit to @fourdevils for stealing the page and to sturmgeist aka zirt for deobfuscation');
+console.log('Credit to @fourdevils for stealing the page and to sturmgeist aka zirt for deobfuscation');
+console.log('Credit to @fourdevils for stealing the page and to sturmgeist aka zirt for deobfuscation');
+console.log('Credit to @fourdevils for stealing the page and to sturmgeist aka zirt for deobfuscation');
+console.log('Credit to @fourdevils for stealing the page and to sturmgeist aka zirt for deobfuscation');
+
+window.oncontextmenu = function () {
+  return false;
+};
+$(document).keydown(function (event) {
+  if (event.keyCode == 123) {
+    return false;
+  } else {
+    if (event.ctrlKey && event.shiftKey && event.keyCode == 73 || event.ctrlKey && event.shiftKey && event.keyCode == 74) {
+      return false;
+    }
+  }
+});
+function audioPlay() {
+  var audioElement = document.getElementById("audio");
+  audioElement.volume = 0.3;
+  audioElement.play();
+}
+function videoPlay() {
+  var audioElement = document.getElementById("video");
+  audioElement.play();
+}
